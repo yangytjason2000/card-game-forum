38b0565a1a5cd93b9350e2c57408ecf99847f7be
yangytjason2000 <yangyt.jason2015@outlook.com>
2024-04-07 15:40:04 -0700

    Added search bar

--- src/app/c/[slug]/layout.tsx
+++ src/app/c/[slug]/layout.tsx
@@ -56,7 +56,7 @@ const Layout = async ({
 		<div className="sm:container max-w-7xl mx-auto h-full pt-12">
 			<div>
 				<div className="grid grid-cols-1 md:grid-cols-3 gay-y-4 md:gap-x-4 py-6">
-					<div className="flex flex-col col-span-2 space-y-6">{children}</div>
+					<ul className="flex flex-col col-span-2 space-y-6">{children}</ul>
 
 					<div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
 						<div className="px-6 py-4">
